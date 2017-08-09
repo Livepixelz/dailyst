6fa6cae10b4b15da57000cdc7b1bc4cf283fa9cc
Livepixelz <contact@livepixelz.com>
2017-08-09 17:40:12 +0200

    removed jquery code from index

--- src/index.js
+++ src/index.js
@@ -3,24 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// $(document).ready(function(){
-//     $('body').on('click', '.btn', function(){
-//
-//         var $button = $(this);
-//         if($button.hasClass('btn-daytime')) {
-//             var timeOfDay = $button.data('timeofday');
-//             $('.body_background').removeClass('active');
-//             $('body').attr('class','');
-//             $('.body--' + timeOfDay).addClass('active');
-//             $('body').addClass(timeOfDay);
-//         } else if ($button.hasClass('btn-display')) {
-//             var displayType = $button.data('list-display');
-//             $('.trackList').attr('class','trackList');
-//             $('.trackList').addClass('display--' + displayType);
-//         }
-//
-//     });
-//
-// });
 ReactDOM.render(<App apiKey={"3750fc3b89c9ff609a6180bcc6ba8669"} username={"okend"} tracks={30} />, document.getElementById('react'));
 registerServiceWorker();
